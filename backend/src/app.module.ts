cde2b6c62ba3eb1e389d532b39b6c6f7ba314d70
Jan Bauer <bauer.jan123@gmail.com>
2021-05-26 21:05:06 +0200

    Added Word entity and DB repository

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeOrmConfig';
+import { T9Repository } from './app.repository';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forFeature([T9Repository]),
   ],
   controllers: [AppController],
   providers: [AppService],

3e37299988282a0fa37d9edc3411027dcd361c6f
Jan Bauer <bauer.jan123@gmail.com>
2021-05-31 18:03:18 +0200

    Added NotFound page

---
+++ frontend/src/containers/NotFoundPage/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components/macro";
+import {withNavigation} from "../../hoc/NavBar";
+
+function NotFoundPage() {
+  return (
+    <>
+      <Wrapper>
+        <Title>
+          4
+          <span role="img" aria-label="Crying Face">
+            😢
+          </span>
+          4
+        </Title>
+        <p>Page not found.</p>
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 320px;
+`;
+
+const Title = styled.div`
+  margin-top: -8vh;
+  font-weight: bold;
+  color: black;
+  font-size: 3.375rem;
+
+  span {
+    font-size: 3.125rem;
+  }
+`;
+
+export default withNavigation(NotFoundPage);

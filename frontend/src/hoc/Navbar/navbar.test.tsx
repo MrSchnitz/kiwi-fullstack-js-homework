f059261ab52eb590bd77e10fe7824effd14cf64d
Jan Bauer <bauer.jan123@gmail.com>
2021-06-04 09:30:52 +0200

    Added HOC tests

---
+++ frontend/src/hoc/Navbar/navbar.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {shallow, ShallowWrapper} from "enzyme";
+import {withNavigation} from "./navbar.component";
+import {configureAppStore} from "../../internals/configureStore";
+import {Provider} from "react-redux";
+
+describe("NavBar HOC component", () => {
+  let wrapper: ShallowWrapper;
+
+  beforeEach(() => {
+    const store = configureAppStore();
+    const NavigationComponent = withNavigation(<div />);
+    wrapper = shallow(
+      <Provider store={store}>
+        <NavigationComponent />
+      </Provider>
+    );
+  });
+
+  it("should render NavBar component", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
